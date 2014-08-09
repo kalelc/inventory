aa7991edd84e503aabc5210eba976d7265eabb38
kalelc <andreskal@gmail.com>
2014-08-09 16:25:29 -0500

    add measure null

--- module/Admin/src/Admin/Model/MeasureTable.php
+++ module/Admin/src/Admin/Model/MeasureTable.php
@@ -81,6 +81,9 @@ class MeasureTable
 
 	public function save(measure $measure)
 	{
+		if(empty($measure->getMeasureType()))
+			$measure->setMeasureType(NULL);
+		
 		$data = array(
 			'specification' => $measure->getSpecification(),
 			'measure_type' => $measure->getMeasureType(),
