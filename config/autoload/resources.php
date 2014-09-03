3b6c90e09baf591f5034b85259bfe6535af3c73e
andreskal <andreskal@gmail.com>
2014-09-03 18:58:12 -0500

    add change into rols

--- config/autoload/resources.php
+++ config/autoload/resources.php
@@ -2,29 +2,29 @@
 return array(
 	'resources' => array(
 		"admin" => array(
-			"app" => array("list", "create", "edit", "remove"),
-			"brand" => array("list", "create", "edit", "remove"),
-			"bank" => array("list", "create", "edit", "remove"),
-			"category" => array("list", "create", "edit", "remove"),
-			"classification" => array("list", "create", "edit", "remove"),
-			"customer" => array("list", "create", "edit", "remove"),
-			"list_price" => array("list", "create", "edit", "remove"),
-			"master_category" => array("list", "create", "edit", "remove"),
-			"measure_type" => array("list", "create", "edit", "remove"),
-			"measure" => array("list", "create", "edit", "remove"),
-			"payment_method" => array("list", "create", "edit", "remove"),
-			"product" => array("list", "create", "edit", "remove"),
-			"serial_name" => array("list", "create", "edit", "remove"),
-			"specification" => array("list", "create", "edit", "remove"),
-			"specification_master" => array("list", "create", "edit", "remove"),
-			"user_type" => array("list", "create", "edit", "remove"),
+			"app",
+			"brand",
+			"bank",
+			"category",
+			"classification",
+			"customer",
+			"list_price",
+			"master_category",
+			"measure_type",
+			"measure",
+			"payment_method",
+			"product",
+			"serial_name",
+			"specification",
+			"specification_master",
+			"user_type",
 			),
 		"security" => array(
-			"rol" => array("create", "edit", "remove"),
-			"user" => array("create", "edit", "remove"),
+			"rol",
+			"user",
 			),
 		"process" => array(
-			"receive_inventory" => array("create"),
+			"receive_inventory",
 			//"out_inventory" => array( "create")
 			)
 		)
